393b34524f7afa97186b8d80dc762ac6eef212a2
Manish <Manish.Singh@Honeywell.com>
2019-01-29 00:54:18 +0530

    Added w_strlen function to return string lenght of wchar_t string

--- src/string_operations.c
+++ src/string_operations.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <wchar.h>
+
+size_t w_strlen(const wchar_t *s)
+{
+   
+   const wchar_t *p = s;
+   
+   
+   while (*p)
+    ++p;
+    return (p - s);
+   
+}
 
 char *trim(char *value, char *s)
 {
